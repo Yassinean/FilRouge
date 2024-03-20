30e5522bf8fcf2b196af7ad2479d8fe554b93a0e
Yassinean <yassinedubraska@gmail.com>
2024-03-20 02:40:17 +0000

    still fixing ajax issue for confirmation fields in register form

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,16 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AccountController extends Controller
 {
     //this function will show us register page 
-    public function register(){
+    public function register()
+    {
         return view('front.account.register');
     }
-    
+
+    //this method will save user
+    public function processRegistration(Request $request)
+    {
+        $validateData = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:5|same:confirm_password',
+            'confirm_password' => 'required',
+        ]);
+
+        if ($validateData) {
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validateData->errors()
+            ]);
+        }
+    }
+
     //this function will show us login page 
-    public function login(){
+    public function login()
+    {
         return view('front.account.login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,6 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/register', [AccountController::class, 'register'])->name('account.register');
+Route::post('/process-registration', [AccountController::class, 'processRegistration'])->name('account.processRegistration');
+Route::get('/login', [AccountController::class, 'login'])->name('account.login');
