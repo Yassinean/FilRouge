3c19442bbbc62d4206c6269d2f2d4d0d412aac0a
Yassine Hanach <yassinedubraska@gmail.com>
2024-04-23 01:31:52 +0100

    finish services interface

---
+++ app/Services/Implementations/CategoryJobService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services\Implementations;
+
+
+use App\Http\Requests\UpdatejobsRequest;
+use App\Models\Job;
+use App\Models\User;
+use App\Repositories\Interfaces\JobsInterface;
+use App\Services\Interfaces\JobsServiceInterface;
+use Illuminate\Http\Request;
+
+class CategoryJobService implements JobsServiceInterface
+{
+
+    public function __construct(protected JobsInterface $repository)
+    {
+
+    }
+
+    public function index(Request $request)
+    {
+        return $this->repository->index($request);
+    }
+
+    public function create()
+    {
+        return $this->repository->create();
+    }
+
+    public function store(User $user)
+    {
+        return $this->repository->store();
+    }
+
+    public function getJob()
+    {
+        return $this->repository->getJob();
+    }
+
+    public function show($id)
+    {
+        return $this->repository->show($id);
+    }
+
+    public function edit($id)
+    {
+        return $this->repository->edit($id);
+    }
+
+    public function update(UpdatejobsRequest $request, $id)
+    {
+        return $this->repository->update($request,$id);
+    }
+
+    public function destroy($id)
+    {
+        return $this->repository->destroy($id);
+    }
+
+    public function applyJob(Request $request)
+    {
+        return $this->repository->applyJob($request);
+    }
+
+    public function saveJob(Request $request)
+    {
+        return $this->repository->saveJob($request);
+    }
+}
--- app/Services/Interfaces/CategoryJobServiceInterface.php
+++ app/Services/Interfaces/CategoryJobServiceInterface.php
@@ -2,25 +2,18 @@
 
 namespace App\Services\Interfaces;
 
-use App\Http\Requests\UpdatejobsRequest;
-use App\Models\Job;
-use App\Models\User;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreCategoryJobRequest;
+use App\Http\Requests\UpdateCategoryJobRequest;
+
 
 interface CategoryJobServiceInterface
 {
-    public function index(Request $request);
+    public function index();
 
-    public function create();
+    public function store(StoreCategoryJobRequest $request);
 
-    public function store(User $user);
-
-    public function getJob();
-    public function show($id);
     public function edit($id);
-    public function update(UpdatejobsRequest $request, $id);
+    public function update(UpdateCategoryJobRequest $request, $id);
     public function destroy($id);
-    public function applyJob(Request $request);
-    public function saveJob(Request $request);
 
 }
